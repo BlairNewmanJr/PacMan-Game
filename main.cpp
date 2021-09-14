55d5c6e3cd7e5022b9b6c5878320612eafa9b875
BlairNewmanJr <71617958+BlairNewmanJr@users.noreply.github.com>
2021-09-14 15:40:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,9 +16,9 @@ using Monster = array<float, 3>;
 static bool replay = false; //check if starts a new game
 static bool over = true; //check for the game to be over
 static float squareSize = 50.0; //size of one square on the game
-static float xIncrement = 0; // x movement on pacman
-static float yIncrement = 0; // y movement on pacman
-static int rotation = 0; // orientation of pacman
+static float xIncrement = 0; // x movement on vaxman
+static float yIncrement = 0; // y movement on vaxman
+static int rotation = 0; // orientation of vaxman
 
 vector<Monster> v_pinkys;
 vector<Monster> v_blinkys;
@@ -37,7 +37,7 @@ static vector<int> obstaclesTop = { 2, 2, 3, 6, 3, 6, 4, 5, 4, 2, 5, 4, 5, 3, 6,
 static vector<int> obstaclesMiddle = { 2, 9, 3, 7, 3, 7, 4, 8, 4, 9, 5, 11, 5, 6, 6, 10, 6, 10, 7, 8, 7, 8, 8, 9, 6, 7, 7, 6, 8, 6, 9, 7, 10, 6, 9, 10, 9, 10, 8, 8, 11, 9, 10, 11, 11, 8, 12, 7, 12, 7, 13, 9 };
 static vector<int> obstaclesBottom = { 2, 10, 3, 13, 3, 13, 4, 12, 5, 12, 6, 13, 6, 13, 7, 11, 8, 11, 9, 13, 9, 13, 10, 12, 11, 12, 12, 13, 12, 13, 13, 10 };
 static deque<float> food = { 1.5, 1.5, 1.5, 2.5, 1.5, 3.5, 1.5, 4.5, 1.5, 5.5, 1.5, 6.5, 1.5, 7.5, 1.5, 8.5, 1.5, 9.5, 1.5, 10.5, 1.5, 11.5, 1.5, 12.5, 1.5, 13.5, 2.5, 1.5, 2.5, 6.5, 2.5, 9.5, 2.5, 13.5, 3.5, 1.5, 3.5, 2.5, 3.5, 3.5, 3.5, 4.5, 3.5, 6.5, 3.5, 8.5, 3.5, 9.5, 3.5, 10.5, 3.5, 11.5, 3.5, 13.5, 4.5, 1.5, 4.5, 4.5, 4.5, 5.5, 4.5, 6.5, 4.5, 7.5, 4.5, 8.5, 4.5, 11.5, 4.5, 12.5, 4.5, 13.5, 5.5, 1.5, 5.5, 2.5, 5.5, 5.5, 5.5, 10.5, 5.5, 11.5, 5.5, 13.5, 6.5, 2.5, 6.5, 3.5, 6.5, 4.5, 6.5, 5.5, 6.5, 7.5, 6.5, 10.5, 6.5, 13.5, 7.5, 5.5, 7.5, 6.5, 7.5, 7.5, 7.5, 9.5, 7.5, 10.5, 7.5, 11.5, 7.5, 12.5, 7.5, 13.5, 8.5, 2.5, 8.5, 3.5, 8.5, 4.5, 8.5, 5.5, 8.5, 7.5, 8.5, 10.5, 8.5, 13.5, 9.5, 1.5, 9.5, 2.5, 9.5, 5.5, 9.5, 10.5, 9.5, 11.5, 9.5, 13.5, 10.5, 1.5, 10.5, 4.5, 10.5, 5.5, 10.5, 6.5, 10.5, 7.5, 10.5, 8.5, 10.5, 11.5, 10.5, 12.5, 10.5, 13.5, 11.5, 1.5, 11.5, 2.5, 11.5, 3.5, 11.5, 4.5, 11.5, 5.5, 11.5, 6.5, 11.5, 8.5, 11.5, 9.5, 11.5, 10.5, 11.5, 11.5, 11.5, 13.5, 12.5, 1.5, 12.5, 6.5, 12.5, 9.5, 12.5, 13.5, 13.5, 1.5, 13.5, 2.5, 13.5, 3.5, 13.5, 4.5, 13.5, 5.5, 13.5, 6.5, 13.5, 7.5, 13.5, 8.5, 13.5, 9.5, 13.5, 10.5, 13.5, 11.5, 13.5, 12.5, 13.5, 13.5 };
-static vector<vector<bool>> bitmap; // 2d image of which squares are blocked and which are clear for pacman to move in 
+static vector<vector<bool>> bitmap; // 2d image of which squares are blocked and which are clear for vaxman to move in 
 bool* keyStates = new bool[256]; // record of all keys pressed 
 int points = 0; // total points collected
 
@@ -111,9 +111,9 @@ void drawLaberynth() {
 }
 
 //Method to check if the food has been eaten
-bool foodEaten(int x, int y, float pacmanX, float pacmanY) {
-	if (x >= pacmanX - 16.0 * cos(359 * M_PI / 180.0) && x <= pacmanX + 16.0 * cos(359 * M_PI / 180.0)) {
-		if (y >= pacmanY - 16.0 * cos(359 * M_PI / 180.0) && y <= pacmanY + 16.0 * cos(359 * M_PI / 180.0)) {
+bool foodEaten(int x, int y, float vaxmanX, float vaxmanY) {
+	if (x >= vaxmanX - 16.0 * cos(359 * M_PI / 180.0) && x <= vaxmanX + 16.0 * cos(359 * M_PI / 180.0)) {
+		if (y >= vaxmanY - 16.0 * cos(359 * M_PI / 180.0) && y <= vaxmanY + 16.0 * cos(359 * M_PI / 180.0)) {
 			return true;
 		}
 	}
@@ -121,11 +121,11 @@ bool foodEaten(int x, int y, float pacmanX, float pacmanY) {
 }
 
 //Method to draw all the food left and delete the ate one
-void drawFood(float pacmanX, float pacmanY) {
+void drawFood(float vaxmanX, float vaxmanY) {
 	deque<float> temp;
 	//check if the food has not been eaten
 	for (int i = 0; i < food.size(); i = i + 2) {
-		if (!foodEaten(food.at(i) * squareSize, food.at(i + 1) * squareSize, pacmanX, pacmanY)) {
+		if (!foodEaten(food.at(i) * squareSize, food.at(i + 1) * squareSize, vaxmanX, vaxmanY)) {
 			temp.push_back(food.at(i));
 			temp.push_back(food.at(i + 1));
 		}
@@ -144,8 +144,8 @@ void drawFood(float pacmanX, float pacmanY) {
 	glEnd();
 }
 
-//Method to draw the pacman character through consicutive circle algorithm
-void drawPacman(float positionX, float positionY, float rotation) {
+//Method to draw the vaxman character through consicutive circle algorithm
+void drawvaxman(float positionX, float positionY, float rotation) {
 	int x, y;
 	glBegin(GL_LINES);
 	glColor3f(1.0, 1.0, 0.0);
@@ -281,7 +281,7 @@ void resetGame() {
 	food = { 1.5, 1.5, 1.5, 2.5, 1.5, 3.5, 1.5, 4.5, 1.5, 5.5, 1.5, 6.5, 1.5, 7.5, 1.5, 8.5, 1.5, 9.5, 1.5, 10.5, 1.5, 11.5, 1.5, 12.5, 1.5, 13.5, 2.5, 1.5, 2.5, 6.5, 2.5, 9.5, 2.5, 13.5, 3.5, 1.5, 3.5, 2.5, 3.5, 3.5, 3.5, 4.5, 3.5, 6.5, 3.5, 8.5, 3.5, 9.5, 3.5, 10.5, 3.5, 11.5, 3.5, 13.5, 4.5, 1.5, 4.5, 4.5, 4.5, 5.5, 4.5, 6.5, 4.5, 7.5, 4.5, 8.5, 4.5, 11.5, 4.5, 12.5, 4.5, 13.5, 5.5, 1.5, 5.5, 2.5, 5.5, 5.5, 5.5, 10.5, 5.5, 11.5, 5.5, 13.5, 6.5, 2.5, 6.5, 3.5, 6.5, 4.5, 6.5, 5.5, 6.5, 7.5, 6.5, 10.5, 6.5, 13.5, 7.5, 5.5, 7.5, 6.5, 7.5, 7.5, 7.5, 9.5, 7.5, 10.5, 7.5, 11.5, 7.5, 12.5, 7.5, 13.5, 8.5, 2.5, 8.5, 3.5, 8.5, 4.5, 8.5, 5.5, 8.5, 7.5, 8.5, 10.5, 8.5, 13.5, 9.5, 1.5, 9.5, 2.5, 9.5, 5.5, 9.5, 10.5, 9.5, 11.5, 9.5, 13.5, 10.5, 1.5, 10.5, 4.5, 10.5, 5.5, 10.5, 6.5, 10.5, 7.5, 10.5, 8.5, 10.5, 11.5, 10.5, 12.5, 10.5, 13.5, 11.5, 1.5, 11.5, 2.5, 11.5, 3.5, 11.5, 4.5, 11.5, 5.5, 11.5, 6.5, 11.5, 8.5, 11.5, 9.5, 11.5, 10.5, 11.5, 11.5, 11.5, 13.5, 12.5, 1.5, 12.5, 6.5, 12.5, 9.5, 12.5, 13.5, 13.5, 1.5, 13.5, 2.5, 13.5, 3.5, 13.5, 4.5, 13.5, 5.5, 13.5, 6.5, 13.5, 7.5, 13.5, 8.5, 13.5, 9.5, 13.5, 10.5, 13.5, 11.5, 13.5, 12.5, 13.5, 13.5 };
 }
 
-//Method to update the movement of the pacman according to the movement keys pressed
+//Method to update the movement of the vaxman according to the movement keys pressed
 void keyOperations() {
 	//get current position
 	float  x = (1.5 + xIncrement) * squareSize;
@@ -332,15 +332,15 @@ void keyOperations() {
 
 //Method to check if the game is over
 void gameOver() {
-	int pacmanX = (int)(1.5 + xIncrement);
-	int pacmanY = (int)(1.5 + yIncrement);
+	int vaxmanX = (int)(1.5 + xIncrement);
+	int vaxmanY = (int)(1.5 + yIncrement);
 
 
 	for (auto& v : v_all_ghosts) {					// Grab each set of instances
 		int z = 0;									// initialize counter to 0
 		for (auto& ghost : v) {					// for every ghost in each set
-			if (pacmanX == (int)(ghost[0]) &&	// pacmanX == ghostX
-				pacmanY == (int)(ghost[1])) {	// pacmanY == ghostY
+			if (vaxmanX == (int)(ghost[0]) &&	// vaxmanX == ghostX
+				vaxmanY == (int)(ghost[1])) {	// vaxmanY == ghostY
 				v.erase(v.begin()+z);		// erase the ghost in the set located at z
 				//OutputDebugStringA("deleted ghost");
 				count_ghosts--;
@@ -420,7 +420,7 @@ void welcomeScreen() {
 	glRasterPos2f(150, 200);
 	while (*message)
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *message++);
-	message = "PACMAN - Original By Patricia Terol";
+	message = "vaxman - Original By Patricia Terol";
 	glColor3f(1, 1, 1);
 	glRasterPos2f(225, 250);
 	while (*message)
@@ -429,7 +429,7 @@ void welcomeScreen() {
 	glRasterPos2f(150, 300);
 	while (*message)
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *message++);
-	message = "To control Pacman use A to go right, D to go left, W to go up and S to go down.";
+	message = "To control vaxman use A to go right, D to go left, W to go up and S to go down.";
 	glRasterPos2f(50, 400);
 	while (*message)
 		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *message++);
@@ -503,7 +503,7 @@ void display() {
 
 			drawLaberynth();
 			drawFood((1.5 + xIncrement) * squareSize, (1.5 + yIncrement) * squareSize);
-			drawPacman(1.5 + xIncrement, 1.5 + yIncrement, rotation);
+			drawvaxman(1.5 + xIncrement, 1.5 + yIncrement, rotation);
 			
 			int it = 0;
 
@@ -559,7 +559,7 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(750, 750);
 	glutInitWindowPosition(500, 50);
-	glutCreateWindow("PACMAN - original by Patricia Terol\nAdapted by Jibran Absarulislam and\nNixon Puertollano");
+	glutCreateWindow("vaxman - original by Patricia Terol\nAdapted by Jibran Absarulislam and\nNixon Puertollano");
 
 	//define all the control functions
 	glutDisplayFunc(display);
